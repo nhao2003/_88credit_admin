65af23f3083fc227313cad14bbe86a9b25f35208
minhphan46 <21522345@gm.uit.edu.vn>
2024-04-30 21:59:56 +0700

    Add dependencies and update page title and favicon

---
+++ src/routes/AppRoutes.js
@@ -0,0 +1,12 @@
+// class AppRoutes definde string constants for all the routes in the application
+
+export default class AppRoutes {
+  static HOME = "/";
+  static LOGIN = "/login";
+  static REGISTER = "/register";
+  static DASHBOARD = "/dashboard";
+  static PROFILE = "/profile";
+  static USERS = "/users";
+  static USER = "/user/:id";
+  static NOT_FOUND = "/not-found";
+}
