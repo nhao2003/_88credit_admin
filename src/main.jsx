b5f3a76f8d7db244a10a11226df86602ca8c5b0b
minhphan46 <21522345@gm.uit.edu.vn>
2024-05-04 10:28:54 +0700

    login page

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AppPages } from "./routes/AppPages";
+import LoginPage from "./modules/login/LoginPage";
 
 const App = () => {
   return (
     <React.StrictMode>
-      <RouterProvider router={AppPages} />
+      {localStorage.getItem("token") === "1234567890" ? (
+        <RouterProvider router={AppPages} />
+      ) : (
+        <LoginPage />
+      )}
     </React.StrictMode>
   );
 };
