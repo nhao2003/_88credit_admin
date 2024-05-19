58db68e590ecafd8f7ef7f28f02188cf4c46516a
minhphan46 <21522345@gm.uit.edu.vn>
2024-05-20 00:17:50 +0700

    add terminal

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import Terminal from "vite-plugin-terminal";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), Terminal()],
+  server: {
+    cors: false,
+    // proxy: {
+    //   "/post": {
+    //     target: "http://172.17.12.163:8080",
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace(/^\/post/, ""),
+    //   },
+    // },
+  },
+});
